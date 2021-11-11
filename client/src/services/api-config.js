1cc893f3853da4e6493c58b2c51556e60d139ab4
srhodges <89659625+srhodges@users.noreply.github.com>
2021-11-11 12:29:53 -0800

    deploy

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const baseUrl = process.env.NODE_ENV === 'production' ? 'https://genuineengineering-api.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
-  baseURL: baseUrl
+  baseURL: baseUrl,
+  headers: {"Access-Control-Allow-Origin": "*"}
 })
 
 export default api;
